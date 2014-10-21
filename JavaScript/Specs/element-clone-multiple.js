9f2400039f6f7c386d2570037ddcc16921228faf
Hristo Chakarov <mail@ickata.net>
2014-10-21 19:51:44 +0300

    Performance test

--- JavaScript/Specs/element-clone-multiple.js
+++ JavaScript/Specs/element-clone-multiple.js
@@ -23,4 +23,38 @@ describe('HTMLElement.cloneMultiple', function () {
       expect( element.cloneMultiple( 10, true ).childNodes[0].childNodes.length )
             .toEqual( element.childNodes.length );
    });
+   
+   var timer = {
+      
+      _timers  : {},
+      
+      start    : function ( id ) {
+         this._timers[ id ] = new Date().getTime();
+      },
+      
+      end      : function ( id ) {
+         return new Date().getTime() - this._timers[ id ];
+      }
+   };
+   
+   it('Is faster than `cloneNode` for large amount of DOM nodes', function () {
+      var clones_num = 20000;
+      
+      timer.start('cloneNode');
+      for ( var i=0; i<clones_num; i += 1 ) {
+         document.body.appendChild( element.cloneNode( true ) );
+      }
+      var time_clone_node = timer.end('cloneNode');
+      
+      timer.start('cloneMultiple');
+      document.body.appendChild( element.cloneMultiple( clones_num, true ) );
+      var time_clone_multiple = timer.end('cloneMultiple');
+      
+      expect( time_clone_multiple < time_clone_node ).toBeTruthy();
+      
+      console.log(
+         '`cloneNode` finished in: ' + time_clone_node + ' miliseconds\n' +
+         '`cloneMultiple` finished in: ' + time_clone_multiple + ' miliseconds'
+      );
+   });
 });
